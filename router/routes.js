6dafd2d534ca1cfa4ab5cf8a0bb97482dd7f8ef7
nodeexam <node@exam.com>
2026-06-03 10:50:48 +0000

    Put erInnlogget on /saker and /saker/opprett

--- router/routes.js
+++ router/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const ticketController = require("../controllers/ticketController");
+const { erInnlogget } = require("../middleware/auth");
 
 router.get("/", (req, res)=> {
     res.render("homepage");
@@ -20,8 +21,8 @@ router.post("/signup", authController.registerUser);
 router.post("/login", authController.loginUser);
 
 //ticketcontroller
-router.get("/saker/opprett", ticketController.visSak);
-router.post("/saker/opprett", ticketController.opprettSak);
-router.get("/saker", ticketController.visAlleSaker);
+router.get("/saker/opprett", erInnlogget, ticketController.visSak);
+router.post("/saker/opprett", erInnlogget, ticketController.opprettSak);
+router.get("/saker", erInnlogget, ticketController.visAlleSaker);
 
 module.exports = router;
